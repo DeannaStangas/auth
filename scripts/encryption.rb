9e92e6db10fa2eede2cf6ec2e426c23f86c1696c
Deanna Stangas <deanna.stangas@kellogg.northwestern.edu>
2025-05-20 15:22:34 +0000

    before second break

--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,11 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+secret = "puppies"
+puts secret
+encrypted_secret = BCrypt::Password.create(secret)
+puts encrypted_secret
 # 2. prepare encrypted string for testing
+user_submitted_secret = "kittens"
+BCrypt::Password.new(encrypted) == secret
 # 3. test secret against prepared encrypted string
